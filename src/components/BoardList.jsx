911237f39e7eed0942ef4e7565955c342e0df3ca
s0y0ungk <soyo2039@gmail.com>
2026-07-02 16:40:27 +0900

    Implement form submission in Write component with axios POST request

--- src/components/BoardList.jsx
+++ src/components/BoardList.jsx
@@ -49,8 +49,8 @@ export default function BoardList() {
           </tr>
         </thead>
         <tbody>
-          {list.map(item => (
-            <Board data={item} />
+          {list.map((item, idx) => (
+            <Board key={idx} data={item} />
           ))}
         </tbody>
       </Table>
